4afca10133bd94e799670b86321a8fff2be81970
FrancoJarc <francoj1612@gmail.com>
2024-04-26 21:29:23 -0300

    Punto 6 hecho. Instanciada la clase pesquero, ejecutado su metodo recolectar() e instanciada la clase bancoDePesca

--- main.php
+++ main.php
@@ -3,13 +3,17 @@
 require_once("arbusto.php");
 require_once("aldeano.php");
 require_once("bancodepesca.php");
+require_once("pesquero.php");
 
 $arbusto1 = new Arbusto();
 $aldeano1= new Aldeano();
 $aldeano1->recolectar($arbusto1);
 
+
+$pesquero1 = new Pesquero();
 $bancoDePesca1= new BancoDePesca();
 
+$pesquero1->recolectar($bancoDePesca1);
 
 
 ?>
--- pesquero.php
+++ pesquero.php
@@ -2,15 +2,27 @@
 
 class Pesquero{
 
-    function __construct(){
+    private $velocidadRecoleccion;
 
+    function __construct(){
+        $this->velocidadRecoleccion=18;
 
     }
 
     public function recolectar(BancoDePesca $banco){
 
+        $tiempo=$banco->getAlimento()/$this->velocidadRecoleccion;
+        $tiempo=ceil($tiempo);
 
+        if($tiempo<=1){
+            echo"Recolecte toda la pesca en ".$tiempo." minuto.";
+        }else{
+            echo"Recolecte toda la pesca en ".$tiempo." minutos.";
+        }
     }
+
+
+
 }
 
 
